7f3538fb5e687983bf0df589a1cf47e37c9e1822
Leopold David <leopolddavid@mac-4.home>
2026-04-22 16:23:53 +0200

    Correction du bug sur les presentations dans le footer

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -19,6 +19,9 @@ export const api = {
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+  const last =
+  data?.events &&
+  [...data.events].sort((a, b) => new Date(b.date) - new Date(a.date))[0];
   const getData = useCallback(async () => {
     try {
       setData(await api.loadData());
@@ -37,6 +40,7 @@ export const DataProvider = ({ children }) => {
       value={{
         data,
         error,
+        last,
       }}
     >
       {children}
